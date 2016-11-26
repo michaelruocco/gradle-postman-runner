bcbbe8fee33bb63273400ad59f8627e293ffc327
Matthias Simon <simon@infonautika.de>
2016-11-26 14:05:07 +0100

    don't install newman when it's already there

--- src/main/java/de/infonautika/postman/task/InstallNewmanTask.java
+++ src/main/java/de/infonautika/postman/task/InstallNewmanTask.java
@@ -1,7 +1,12 @@
 package de.infonautika.postman.task;
 
+import com.moowork.gradle.node.NodeExtension;
 import com.moowork.gradle.node.task.NpmSetupTask;
 import com.moowork.gradle.node.task.NpmTask;
+import org.gradle.api.Action;
+import org.gradle.api.Project;
+
+import java.io.File;
 
 import static de.infonautika.postman.PostmanRunnerPlugin.GROUP_NAME;
 import static java.util.Collections.singletonList;
@@ -15,5 +20,18 @@ public class InstallNewmanTask extends NpmTask {
         setNpmCommand("install");
         setArgs(singletonList("newman"));
         dependsOn(NpmSetupTask.NAME);
+
+        getProject().afterEvaluate(addNewmanDirToOutputs());
+    }
+
+    private Action<? super Project> addNewmanDirToOutputs() {
+        return new Action<Project>() {
+            @Override
+            public void execute(Project project) {
+                getOutputs().dir(
+                        new File(getProject().getExtensions().getByType(NodeExtension.class).getNodeModulesDir(),
+                                "node_modules/newman"));
+            }
+        };
     }
 }
