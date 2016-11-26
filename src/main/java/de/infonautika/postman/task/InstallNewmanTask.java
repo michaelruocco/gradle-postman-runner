d5d9f0ab2f008d5a742a600b11f337eab207abde
Matthias Simon <simon@infonautika.de>
2016-11-26 17:32:51 +0100

    compatibility with previous gradle version

--- src/main/java/de/infonautika/postman/task/InstallNewmanTask.java
+++ src/main/java/de/infonautika/postman/task/InstallNewmanTask.java
@@ -3,8 +3,7 @@ package de.infonautika.postman.task;
 import com.moowork.gradle.node.NodeExtension;
 import com.moowork.gradle.node.task.NpmSetupTask;
 import com.moowork.gradle.node.task.NpmTask;
-import org.gradle.api.Action;
-import org.gradle.api.Project;
+import org.gradle.api.tasks.OutputDirectory;
 
 import java.io.File;
 
@@ -20,18 +19,11 @@ public class InstallNewmanTask extends NpmTask {
         setNpmCommand("install");
         setArgs(singletonList("newman"));
         dependsOn(NpmSetupTask.NAME);
-
-        getProject().afterEvaluate(addNewmanDirToOutputs());
     }
 
-    private Action<? super Project> addNewmanDirToOutputs() {
-        return new Action<Project>() {
-            @Override
-            public void execute(Project project) {
-                getOutputs().dir(
-                        new File(getProject().getExtensions().getByType(NodeExtension.class).getNodeModulesDir(),
-                                "node_modules/newman"));
-            }
-        };
+    @OutputDirectory
+    public File getNewmanOutputDir() {
+        return new File(getProject().getExtensions().getByType(NodeExtension.class).getNodeModulesDir(),
+                "node_modules/newman");
     }
 }
