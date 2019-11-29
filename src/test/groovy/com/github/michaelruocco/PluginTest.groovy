491a13f6d9fe5a1cbc14fda666284ab64e388d76
Michael Ruocco <michael.ruocco@hotmail.com>
2019-11-29 09:29:54 +0000

    adds test for basic functionality of running a collection with an environment file

--- src/test/groovy/com/github/michaelruocco/PluginTest.groovy
+++ src/test/groovy/com/github/michaelruocco/PluginTest.groovy
@@ -1,13 +1,31 @@
 package com.github.michaelruocco
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import org.junit.Rule
 import spock.lang.Specification
 import org.gradle.testkit.runner.GradleRunner
 
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import static com.github.tomakehurst.wiremock.client.WireMock.get
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class PluginTest extends Specification {
 
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8081);
+
     def "can execute postman collection with environment file"() {
+        given:
+        stubFor(get(urlEqualTo("/gradle-postman-runner-test"))
+                .withHeader("Accept", equalTo("application/json"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"field1\":\"ONE\",\"field2\":2}")));
+
         when:
         def result = GradleRunner.create()
                 .withProjectDir(new File('src/test/resources/test-build'))
@@ -16,7 +34,6 @@ class PluginTest extends Specification {
                 .build()
 
         then:
-        println result.output
         result.task(":postman").outcome == SUCCESS
     }
 
