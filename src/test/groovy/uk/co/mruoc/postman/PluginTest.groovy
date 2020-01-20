d92d9b98fc7d8c680cdbb8493a50ec712b2eb2ca
Michael Ruocco <michael.ruocco@hotmail.com>
2020-01-20 16:33:20 +0000

    updates tests to generate build.gradle file rather than using external files

--- src/test/groovy/uk/co/mruoc/postman/PluginTest.groovy
+++ src/test/groovy/uk/co/mruoc/postman/PluginTest.groovy
@@ -1,7 +1,9 @@
 package uk.co.mruoc.postman
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule
+import org.apache.commons.io.FileUtils
 import org.junit.Rule
+import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 import org.gradle.testkit.runner.GradleRunner
 
@@ -15,25 +17,50 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 class PluginTest extends Specification {
 
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(8081);
+    private TemporaryFolder testProjectDir = new TemporaryFolder()
+
+    @Rule
+    private WireMockRule wireMockRule = new WireMockRule(8081)
+
+    private def buildFile
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+        buildFile << """
+            plugins {
+                id 'com.moowork.node' version '1.3.1'
+                id 'com.github.michaelruocco.gradle-postman-runner'
+            }
+        """
+
+        FileUtils.copyDirectory(new File("src/test/resources"), testProjectDir.root)
+    }
 
     def "can execute postman collection with environment file"() {
         given:
+        buildFile << """
+            postman {
+                collections = fileTree(dir: '.', include: '*postman_collection.json')
+                environment = file('./local.postman_environment.json')
+            }
+        """
+
         stubFor(get(urlEqualTo("/gradle-postman-runner-test"))
                 .withHeader("Accept", equalTo("application/json"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
-                        .withBody("{\"field1\":\"ONE\",\"field2\":2}")));
+                        .withBody("{\"field1\":\"ONE\",\"field2\":2}")))
 
         when:
         def result = GradleRunner.create()
-                .withProjectDir(new File('src/test/resources/test-build'))
+                .withProjectDir(testProjectDir.root)
                 .withArguments('postman')
                 .withPluginClasspath()
                 .build()
 
         then:
+        println result.output
         result.task(":postman").outcome == SUCCESS
     }
 
